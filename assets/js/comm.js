ca11635cfa92b4d0c4e56a2149ec2e122111fea4
yunheeAhn <ayh95944674@gmail.com>
2023-12-26 18:16:21 +0900

    231226

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,7 +1,8 @@
-const swiper = new Swiper('.mySwiper', {
+new Swiper('.mySwiper', {
   // Optional parameters
-  direction: 'vertical',
   loop: true,
+  spaceBetween: 0,
+  
 
   // If we need pagination
   pagination: {
@@ -20,24 +21,26 @@ const swiper = new Swiper('.mySwiper', {
   },
 });
 
-// const swiper2 = new Swiper('.swiper2', {
-//   // Optional parameters
-//   direction: 'vertical',
-//   loop: true,
+new Swiper('.evnt-bnr', {
+  // Optional parameters
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView : 1,
+  slidesPerGroup : 1,
 
-//   // If we need pagination
-//   pagination: {
-//     el: '.swiper-pagination',
-//   },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-//   // Navigation arrows
-//   navigation: {
-//     nextEl: '.swiper-button-next',
-//     prevEl: '.swiper-button-prev',
-//   },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-//   // And if we need scrollbar
-//   scrollbar: {
-//     el: '.swiper-scrollbar',
-//   },
-// });
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
