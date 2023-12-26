2ae31f859b7f685504493d20fdf83b58c371e6ec
yunheeAhn <ayh95944674@gmail.com>
2023-12-26 14:56:13 +0900

    오류 확인중

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,28 +1,43 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    // direction: 'vertical',
-    slidesPerView: 1,
-    spaceBetween: 0,
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    }
-  });
+const swiper = new Swiper('.mySwiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
 
-  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-  
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+
+// const swiper2 = new Swiper('.swiper2', {
+//   // Optional parameters
+//   direction: 'vertical',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
+
+//   // And if we need scrollbar
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
+// });
